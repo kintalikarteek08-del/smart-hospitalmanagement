746bc78100d083fbbb3d5c613789ac4142bad977
test <test@gmail.com>
2026-06-01 22:40:20 +0530

    Fix ALLOWED_HOSTS for Railway

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-!de*9-m@4d2kh9!63qj@op9ar^&z3tf+udyy1bcb9!bfsk7$xs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "smart-hospitalmanagement-production.up.railway.app",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
